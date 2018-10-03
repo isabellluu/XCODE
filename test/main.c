e92bd1836dd5ef11eaf5f1295c1f2be2084c8c1c
Luu, Isabel <1849971@00-63149.local>
2018-10-03 09:43:39 -0700

    Initial Commit

---
+++ test/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  test
+//
+//  Created by Luu, Isabel on 10/3/18.
+//  Copyright © 2018 Luu, Isabel. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
